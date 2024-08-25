c392a97f39f2ccc00cf6a68b958b0ae37d2c9e11
Ekramul <mdekramulhassan168@gmail.com>
2024-08-25 10:59:22 +0600

    project setup and shaden ui added

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/pages/navbar/Navbar";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default MainLayout;
